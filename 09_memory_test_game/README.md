3b624db7a212d74eb298a476c0b4fdb6390119fa
Nooha Abdul Nasar <nooha.nasar456@gmail.com>
2023-09-05 00:06:12 +0530

    Complete memory test website

---
+++ 09_memory_test_game/README.md
@@ -0,0 +1,13 @@
+# Memory Test Challenge
+Designed to test and improve the memory skills of users. This project offers a fun and interactive way to challenge and enhance cognitive abilities related to memory retention and recall.
+
+### How to play?
+* Start the game by pressing any key.
+* Simon generates a sequence of colored buttons with sound.
+* Repeat the sequence by clicking the buttons in order.
+* If correct, advance to the next level.
+* or else made mistake, it's "Game Over"!.
+
+🖥 Site: [Test your memory][def]
+
+[def]: https://memory-test-challenge.netlify.app/
---
+++ 09_memory_test_game/game.js
@@ -0,0 +1,85 @@
+var buttonColors =["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern= [];
+var started = false;
+var level = 0;
+
+$(document).keydown(function(){
+    if (!started) {
+        $("#level-title").text("Level "+ level);
+        nextSequence();
+        started =true;
+    }
+})
+
+$(".btn").on("click", function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+})
+
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel]===gamePattern[currentLevel]) {
+        console.log("success");
+
+        if(userClickedPattern.length===gamePattern.length){
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else{
+        console.log("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        
+        $("#level-title").text("Game Over at Level "+ level+", Press Any Key to Restart");
+        
+        startOver();
+    }
+}
+
+function nextSequence(){
+
+    userClickedPattern=[];
+    level++;
+
+    $("#level-title").text("Level "+ level);
+    var randomNumber= Math.floor(Math.random()*4);
+    var randomChoosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChoosenColor);
+    $("#"+ randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChoosenColor + ".mp3");
+    audio.play();
+
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function() {
+      $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
+    
+function startOver(){
+    $(document).keydown(function(){
+        if (started) {
+            level=0;
+            gamePattern=[];
+            started = false;
+            nextSequence();
+        }
+    })
+}    
