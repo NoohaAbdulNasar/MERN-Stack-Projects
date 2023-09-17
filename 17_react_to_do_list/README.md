7d7b72d84416019f549fffd9fe357a310c7aeacb
Nooha Abdul Nasar <nooha.nasar456@gmail.com>
2023-09-17 11:43:40 +0530

    Organize the code

---
+++ 17_react_to_do_list/README.md
@@ -0,0 +1,6 @@
+# To-do List React App
+This is a simple to-do list application built with React. It allows users to input tasks and add them to a to-do list.
+
+🖥 Site: [To-do list][def]
+
+[def]: https://to-do-react-app/
---
+++ 17_react_to_do_list/src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event){
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItems(){
+    setItems((prevItems)=> [...prevItems, inputText]);
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button onClick={addItems}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item)=> <li>{item}</li> )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
